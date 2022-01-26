c8b4865d85343c52259949159982fa95f82b616e
tpillon <thibaut.pillon@ubikey.fr>
2022-01-26 14:19:14 +0100

    second TD

--- app/src/main/java/fr/tpillon/calculator_td2/ComputeActivity.java
+++ app/src/main/java/fr/tpillon/calculator_td2/ComputeActivity.java
@@ -3,6 +3,8 @@ package fr.tpillon.calculator_td2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ComputeActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class ComputeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compute);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.submit_menu, menu);
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/fr/tpillon/calculator_td2/MainActivity.java
+++ app/src/main/java/fr/tpillon/calculator_td2/MainActivity.java
@@ -3,6 +3,7 @@ package fr.tpillon.calculator_td2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button computeButton = findViewById(R.id.compute_button);
+        Button previousButton = findViewById(R.id.previous_button);
+
+        computeButton.setOnClickListener(view -> openComputeActivity());
+        previousButton.setOnClickListener(view -> openPreviousActivity());
+    }
+
+    private void openPreviousActivity() {
+    }
+
+    private void openComputeActivity() {
     }
 }
